b70c6ce80e57bef3c3082aa550653b5e868c9b86
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2021-01-14 22:18:33 -0300

    electron setup with nodemon

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron');
+require('dotenv').config();
 const path = require('path');
 const url = require('url');
 
@@ -11,8 +12,13 @@ function createWindow() {
     },
   });
 
-  win.loadFile(path.join(__dirname, '..', 'build', 'index.html'));
-  win.removeMenu();
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, '..', 'build', 'index.html'),
+      protocol: 'file:',
+      slashes: true,
+    }),
+  );
 }
 
 app.whenReady().then(createWindow);

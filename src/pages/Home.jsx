6991a47356ba2a1d543f40dc4ceae6905669f62b
GustavoBonfimS <gustavo.hbonfim@outlook.com>
2021-02-06 16:23:23 -0300

    Recharts, bootstrap css set up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,11 @@ import AppMenu from '../components/AppMenu';
 export default function Home() {
   return (
     <>
-      <AppMenu title='Home' />
-      <h1>Pagina inicial</h1>
+      <AppMenu title='Pagina Inicial' />
+      <h1>Visão geral</h1>
+      <div className='container'>
+        <h2 className='text-muted'>teste bootstrap</h2>
+      </div>
     </>
   );
 }
